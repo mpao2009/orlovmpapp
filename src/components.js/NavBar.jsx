3d6468316bc0a5213a913e83089c7bad883aa3eb
mpao2009 <mpao2009@gmail.com>
2022-05-31 20:13:27 -0300

    Add NavBar

---
+++ src/components.js/NavBar.jsx
@@ -0,0 +1,27 @@
+
+const NavBar = () => {
+  return (
+    <>
+     <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">TIENDA NINTENDO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Productos</a>
+        <a class="nav-link" href="#">Carrito</a>
+        <a class="nav-link" href="#">Contacto</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+    </>
+  )
+}
+
+export default NavBar
